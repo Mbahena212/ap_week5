d3e144913563278e16911af6c412a639c404a2d4
Mbahena212 <Miguelitobahena@gmail.com>
2025-11-06 18:46:45 +0000

    extracting information

--- week5_bellRinger.py
+++ week5_bellRinger.py
@@ -12,15 +12,25 @@ last_a_index = print(magic.rindex('a'))
 
 # Advanced Slicing:
 # Given the string alphabet = 'abcdefghijklmnopqrstuvwxyz',
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
 # a. Extract the letters 'hij'.
+hij = print(alphabet.index('hij'))
+hij2 = print(alphabet[7:10])
 # b. Extract every second letter starting from 'a' to 'm'.
 # c. Reverse the entire string using slicing.
+n_index = print(alphabet.index('m'))
+everysecond = print(alphabet[0:13:2])
 
+reversed_alphabet = print(alphabet[ : : -1])
+I_have_a_dream = "And when this happens, and when we allow freedom ring, when we let it ring from every village and every hamlet, from every state and every city, we will be able to speed up that day when all of God's children, Black men and white men, Jews and Gentiles, Protestants and Catholics, will be able to join hands and sing in the words of the old Negro spiritual: Free at last. Free at last. Thank God almighty, we are free at last.
+I_have_a_dream =
 # Problem Set 2: Extracting Information
 # From Descriptions:
 # Extract the name of the famous personality from the quote "Ask not what your country can do for you — ask what you can do for your country. - John F. Kennedy"
-
-# Manipulating Words:
+famous_quote = "ask not what your country can do for you"
+john_f_kennedy = print(famous_quote.find("john f. kennedy"))
+extracted_name = print(famous_quote [83:])
+# Manipulating Words:s
 # Given the string info = "Python is fun. Fun is good. Good is subjective.",
 # a. Extract the word 'subjective' without knowing its exact position.
 # b. Extract every third word.
